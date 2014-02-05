8e619e50e1af2b2d27118a3000cb47c9f3287a73
Tomohiro TAIRA <tomohiro.t@gmail.com>
2014-02-05 10:45:16 +0900

    Define plugin information

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_gantt_view_overwrite do
+  name 'Redmine Gantt View Overwrite plugin'
+  author 'Tomohiro TAIRA'
+  description 'This is a gantt view overwrite plugin for Redmine'
+  version '0.0.4'
+  url 'http://git.es.occ.co.jp/redmine/redmine_gantt_view_overwrite'
+  author_url 'https://github.com/Tomohiro'
+end
